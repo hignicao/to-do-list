ce97571466ad4f0266360edfb28ebfb017de6cc7
Hugo Ignacio <hugo.ignacio1998@gmail.com>
2025-07-19 02:55:41 -0300

    fix: add password confirmation back

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,15 +1,31 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError as DjangoValidationError
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ['id', 'username', 'email', 'password', 'password2']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "As senhas não correspondem."})
+
+        if User.objects.filter(email=attrs['email']).exists():
+            raise serializers.ValidationError({"email": "Este email já está em uso."})
+
+        return attrs
 
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
+            email=validated_data['email'],
             password=validated_data['password']
         )
         return user
